9e0ccbd49585306748233854eb8f91dca94f06d3
Ahmed Salah <ahmed22502@gmail.com>
2020-02-12 21:26:40 +0200

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,7 +8,6 @@ class User
 {
     public static function Signup()
     {
-
         if (isset($_POST['Submit'])) {
             $email = $_POST['Email'];
             $password = $_POST['pwd'];
@@ -19,10 +18,8 @@ class User
                         if (strlen($password) >= 8 && strlen($password) <= 64) {
                             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                                 database::query("INSERT INTO login VALUES ('',:name,:email,:hash_password)", array(':name' => $name, ':email' => $email, ':hash_password' => password_hash($password, PASSWORD_BCRYPT)));
-
                             } else {
                                 loadView('retry');
-
                             }
                         }
                     }
@@ -32,33 +29,37 @@ class User
             }
         }
         return header('Location: /register');
-
-
     }
 
 
     public static function logging()
     {
-        if (isset($_POST['email']) && isset($_POST['password'])) {
-            $email = trim($_POST['email']);
-            $password = trim($_POST['password']);
-            $user = database::query("SELECT * FROM login WHERE email=:email", array(":email" => $email));
-            if ($user) {
-                if (password_verify($password, database::query("SELECT hash_password FROM login WHERE email=:email", array(":email" => $email))[0]['hash_password'])) {
-                    $_SESSION['loggedin'] = TRUE;
-                    $_SESSION['email'] = $_POST['email'];
-                    $_SESSION['id'] = $user[0]["Id"];
-                    $response = array("message"=>"you are logged in");
-                    header('location: /profile');
-                    return  json_encode($response);
+        $requestInput = file_get_contents('php://input');
 
-                }
-                header("HTTP/1.1 401 Unauthorized");
-                exit;
+        // Converts it into a PHP object
+        $requestData = json_decode($requestInput);
 
-            }
-        }
+        exit(json_encode($requestData));
 
+        // if (isset($_POST['email']) && isset($_POST['password'])) {
+            //     $email = trim($_POST['email']);
+        //     $password = trim($_POST['password']);
+
+        //     // $user = database::query("SELECT * FROM login WHERE email=:email", array(":email" => $email));
+        //     // if ($user) {
+        //     //     if (password_verify($password, database::query("SELECT hash_password FROM login WHERE email=:email", array(":email" => $email))[0]['hash_password'])) {
+        //     //         $_SESSION['loggedin'] = TRUE;
+        //     //         $_SESSION['email'] = $_POST['email'];
+        //     //         $_SESSION['id'] = $user[0]["Id"];
+        //     //         $response = array("message"=>"you are logged in");
+        //     //         return  json_encode($response);
+
+        //     //     }
+        //     //     header("HTTP/1.1 401 Unauthorized");
+        //     //     exit;
+
+        //     // }
+        // }
     }
 
 
@@ -71,18 +72,14 @@ class User
 
     public static function update($id)
     {
-
         if (isset($_POST['update'])) {
             $age = $_POST['age'];
             $Bio = $_POST['bio'];
             if (database::query("SELECT * FROM user_data WHERE user_id=:id ", array(':id' => $id))) {
                 database::query('UPDATE  user_data SET age=:age,bio=:bio WHERE user_id=:id', array(':age' => $age, ':bio' => $Bio, ':id' => $id));
-
             } else {
                 database::query("INSERT INTO user_data  VALUES ('',:bio,:user_id,:age)   ", array(':age' => $age, ':bio' => $Bio, ':user_id' => $id));
-
             }
         }
     }
-
 }
